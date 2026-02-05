4fb812602c1771c44c607fad7fbebf571768e96a
kevledex <kevinledesma014@gmail.com>
2026-02-05 07:51:32 -0500

    Utilizando reduce en ejercicio 2

--- src/typescript/Ejercicio2.tsx
+++ src/typescript/Ejercicio2.tsx
@@ -1,23 +1,19 @@
 export const Ejercicio2 = () => {
 
-    const arrreglo: number[] = [5, 5, 8, 2, 10, 9, 1];
+    const arreglo: number[] = [5, 5, 8, 2, 10, 9, 1];
 
     const sumarArreglo = (numeros: number[]): number => {
-      let total = 0;
-
-      for (let index = 0; index < numeros.length; index++) {
-
-        total += numeros[index];
-      }
-    return total;
+      //acum es donde se gaurda la suma
+      //el valor es el numero actual y 0 el numero que inicia
+      return numeros.reduce((acum, valor) => acum + valor, 0)
   }
 
   return (
     <div>
         <h3>Ejercicio 2</h3>
-        <span>Numeros del arreglo: {arrreglo.join(", ")}</span>
+        <span>Numeros del arreglo: {arreglo.join(", ")}</span>
         <br /><br />
-        <span>La suma del arreglo es: {sumarArreglo(arrreglo)}</span>
+        <span>La suma del arreglo es: {sumarArreglo(arreglo)}</span>
     </div>
   )
 }
